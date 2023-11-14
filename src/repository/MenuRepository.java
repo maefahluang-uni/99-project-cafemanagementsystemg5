c7f9fd334c79617ffb36c62bff36ae6353a5cc9f
Thanakrit-Sittipornrattanamanee <122962242+Thanakrit-Sittipornrattanamanee@users.noreply.github.com>
2023-11-14 14:09:20 +0000

    test เพราะเหมือนพอตระเบิด

--- src/repository/MenuRepository.java
+++
@@ -1,6 +0,0 @@
-package th;
-
-public interface MenuRepository {
-
-    
-} 
--- src/repository/StockRepository.java
+++ src/repository/StockRepository.java
@@ -1,14 +1,8 @@
 package th;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.h2.db.model.EmployeeEntity;
-
-
 @Repository
-public interface AdminRepository 
-			extends CrudRepository<AdminEntity, Long> {
-
+public interface StockRepository extends CrudRepository<Stock, Long> {
+    // Define any custom query methods you may need here
 }
-  
