1d91b1f95e78cf896555fb3b0e8a41768f570220
Thanakrit Sittipornrattanamanee <6531503105@lamduan.mfu.ac.th>
2023-11-14 21:21:25 +0700

    test stock page

---
+++ src/Controller/StockController.java
@@ -0,0 +1,91 @@
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StockController {
+
+    @Autowired
+    StockRepository repository;
+
+    
+    public StockController(StockRepository repository) {
+        this.repository = repository;
+    }
+
+    //TODO: add initBinder for date format
+    @InitBinder
+    public final void initBinderUsuariosFormValidator(final WebDataBinder binder, final Locale locale) {
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", locale);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+
+    @GetMapping("/stocks")
+    public String listStocks(Model model) {
+        // TODO: list all concerts
+        model.addAttribute("stocks", repository.findAll());
+        // TODO: return a template to list concerts
+        return "list-stock";
+    }
+
+   
+
+    @GetMapping("/add-stock")
+    public String addStock(Model model) {
+        // TODO: pass blank concert to a form
+        model.addAttribute("stock", new Stock());
+        // TODO: return a template for concert form
+        return "add-edit-stock";
+    }
+
+    @PostMapping("/stocks")
+    public String saveStock(@ModelAttribute Stock stock) {
+        // TODO: add concert to DB
+        repository.save(stock);
+        // TODO: redirect to list concerts
+        return "redirect:/stocks";
+    }
+
+    @GetMapping("/stocks/{id}")
+    public String getStock(Model model, @PathVariable Long id) {
+        //TODO: find concert by id
+        Stock stock = repository.findById(id).get();
+        // TODO: list all concerts
+        model.addAttribute("stock", stock);
+        // TODO: return a template for concert form
+        return "add-edit-stock";
+    }
+
+
+    @GetMapping("/delete-stock/{id}")
+    public String deleteStock(@PathVariable long id) {
+        // TODO: delete concert from DB
+        repository.deleteById(id);
+        // TODO: redirect to list concerts
+        return "redirect:/sttocks";
+    }
+
+    
+    @GetMapping("/delete-stock")
+    public String removeAllStocks() {
+        // delete all employees 
+        repository.deleteAll();
+        return "redirect:/stocks";
+    }
+
+}
--- src/main/java/th/mfu/Domain/Stock.java
+++ src/main/java/th/mfu/Domain/Stock.java
@@ -1,41 +1,37 @@
 package th.mfu.Domain;
 
-import java.sql.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Column;
+import java.util.Date;
 
-    @Entity
+@Entity
 public class Stock {
+
     @Id
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private int  value;
+    private String value;
     private Date date;
 
-
-    public Stock(Long id, String name, int value, Date date) {
-        this.id = id;
-        this.name = name;
-        this.value = value;
-        this.date = date;
+    public Long getId() {
+        return id;
     }
-    
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public int getValue() {
+    public String getValue() {
         return value;
     }
-    public void setValue(int value) {
+    public void setValue(String value) {
         this.value = value;
     }
     public Date getDate() {
@@ -44,17 +40,4 @@ public class Stock {
     public void setDate(Date date) {
         this.date = date;
     }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
-    
-
 }
-
-
