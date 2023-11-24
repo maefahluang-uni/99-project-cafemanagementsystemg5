cbd369c6ef92c6e2b456c0dfdbaccf5c404f8117
UTCHANUN UTTAMA <6531503101@lamduan.mfu.ac.th>
2023-11-24 23:53:18 +0700

    Barista order

---
+++ src/main/resources/static/js/barista-order.js
@@ -0,0 +1,10 @@
+function confirmOrders (){
+    document.getElementById('confirm-btn').addEventListener('click' , removeItem);
+}
+confirmOrders();
+
+function removeItem() {
+    var elementToDelete = document.getElementById('drink-receipt');
+    elementToDelete.remove();
+    
+}
